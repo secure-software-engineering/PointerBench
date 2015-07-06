f69ac7eb6ad03fe02efeb37875215cd9bac851e4
Lisa Nguyen <lisa.nguyen@cased.de>
2015-07-06 09:30:05 +0200

    added call stack test case

---
+++ AliasBenchmark/src/cornerCases/CallStack1.java
@@ -0,0 +1,30 @@
+package cornerCases;
+
+import benchmark.internal.Benchmark;
+import benchmark.objects.A;
+
+/*
+ * @testcase CallStack1
+ * @version 1.0
+ * @author Johannes Späth, Nguyen Quang Do Lisa (Secure Software Engineering Group, Fraunhofer Institute SIT)
+ * 
+ * @description Context difference in call stack
+ * 
+ */
+public class CallStack1 {
+
+	private static A getA() {
+		Benchmark.alloc(1);
+		A x = new A();
+		return x;
+	}
+
+	public static void main(String[] args) {
+
+		A a = getA();
+		A b = getA();
+		Benchmark
+				.test("a",
+						"{allocId:1, mayAlias:[a,b], notMayAlias:[], mustAlias:[a,b], notMustAlias:[]}");
+	}
+}
