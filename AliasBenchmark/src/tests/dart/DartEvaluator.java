579a0a351cb8b6cef6eb478d189bf32513ea627a
Johannes Spaeth <joh.spaeth@googlemail.com>
2015-12-03 13:41:50 +0100

    one updates

--- AliasBenchmark/src/tests/dart/DartEvaluator.java
+++ AliasBenchmark/src/tests/dart/DartEvaluator.java
@@ -13,6 +13,7 @@ import soot.SootField;
 import soot.SootMethod;
 import soot.Type;
 import soot.Unit;
+import soot.accesspath.FieldWithStmt;
 import soot.jimple.infoflow.solver.cfg.BackwardsInfoflowCFG;
 import soot.jimple.infoflow.solver.cfg.InfoflowCFG;
 import soot.jimple.toolkits.ide.icfg.BiDiInterproceduralCFG;
@@ -23,122 +24,127 @@ import benchmark.internal.QueryInfo;
 import dart.AliasFinder;
 import dart.DartContext;
 import dart.accesspath.AccessPath;
-import dart.accesspath.FieldWithStmt;
 import dart.cache.AliasResults;
 import dart.context.AllCallersRequester;
 
-public class DartEvaluator extends Evaluator{
-	public DartEvaluator(QueryInfo queryInfo) {
-		super(queryInfo);
-	}
+public class DartEvaluator extends Evaluator {
+  protected AliasFinder dart;
+  protected InfoflowCFG cfg;
+
+  public DartEvaluator(QueryInfo queryInfo) {
+    super(queryInfo);
+  }
 
 
-	private void createDart() {
-		AliasFinder.TYPE_CHECKING = false;
-		cfg = new InfoflowCFG();
-		BackwardsInfoflowCFG bwcfg = new BackwardsInfoflowCFG(cfg);
-		DartContext context = new DartContext(cfg, bwcfg);
-		dart = new AliasFinder(context);
-	}
+  private void createDart() {
+    dart = null;
+    cfg = new InfoflowCFG();
+    BackwardsInfoflowCFG bwcfg = new BackwardsInfoflowCFG(cfg);
+    DartContext context = new DartContext(cfg, bwcfg);
+    context.budgetInMilliSeconds = 60000;
+    context.startTime = System.nanoTime();
+    dart = new AliasFinder(context);
+    AliasFinder.setTypeChecking(true);
+  }
 
-	@Override
-	protected boolean alias(String l) {
-		createDart();
-		AliasResults res1 = dart.findAliasAtStmt(parse(testVariable),  testStmt, new AllCallersRequester<BiDiInterproceduralCFG<Unit,SootMethod>>(cfg));
-		System.out.println(parse(testVariable));
-		System.out.println(res1);
-		if(res1.isEmpty()){
-			return false;
-		}
+  @Override
+  protected boolean alias(String l) {
+    createDart();
+    AliasResults res1 =
+        dart.findAliasAtStmt(parse(testVariable), testStmt,
+            new AllCallersRequester<BiDiInterproceduralCFG<Unit, SootMethod>>(cfg));
+    System.out.println(res1);
+    if (res1.isEmpty()) {
+      return false;
+    }
 
-		createDart();
-		AliasResults res2 = dart.findAliasAtStmt(parse(l),  testStmt, new AllCallersRequester<BiDiInterproceduralCFG<Unit,SootMethod>>(cfg));
-		System.out.println(parse(l) );
-		System.out.println(res2);
-		System.out.println("");
-		if(res2.isEmpty())
-			return false;
-		for(Pair<Unit, AccessPath> r1 : res1.keySet()){
-			Unit alloc1 = r1.getO2().getSourceStmt();
-			for(Pair<Unit, AccessPath> r2 : res2.keySet()){
-				Unit alloc2 = r2.getO2().getSourceStmt();
-				if(alloc1 != null && alloc1.equals(alloc2))
-					return true;
-			}
-		}
-		return false;
-	}
-
-
-	private AccessPath parse(String l) {
-		return stringToAccessPath(this.method.getActiveBody().getLocals(), l);
-	}
+    createDart();
+    AliasResults res2 =
+        dart.findAliasAtStmt(parse(l), testStmt,
+            new AllCallersRequester<BiDiInterproceduralCFG<Unit, SootMethod>>(cfg));
+    if (res2.isEmpty())
+      return false;
+    for (Pair<Unit, AccessPath> r1 : res1.keySet()) {
+      Unit alloc1 = r1.getO2().getSourceStmt();
+      for (Pair<Unit, AccessPath> r2 : res2.keySet()) {
+        Unit alloc2 = r2.getO2().getSourceStmt();
+        if (alloc1 != null && alloc1.equals(alloc2))
+          return true;
+      }
+    }
+    return false;
+  }
 
 
-	@Override
-	protected int getPointsToSize() {
-		createDart();
-		AliasResults res1 = dart.findAliasAtStmt(parse(testVariable),  testStmt, new AllCallersRequester<BiDiInterproceduralCFG<Unit,SootMethod>>(cfg));
-		if(res1 == null)
-			return 0;
-		Set<Unit> allocSites = new HashSet<>();
-		for(Pair<Unit,AccessPath> o : res1.keySet()){
-			allocSites.add(o.getO2().getSourceStmt());
-		}
-		return allocSites.size();
-	}
-	
-	
-	public static AccessPath stringToAccessPath(Chain<Local> locals, String arg) {
-		for (Local l : locals) {
-			if (arg.contains(".")) {
-				return findFieldsAndLocals(locals, arg);
-			} else if (l.toString().equals(arg)) {
-				return new AccessPath(l, l.getType());
-			}
-		}
-
-		throw new RuntimeException("Could not parse String to access path");
-	}
+  private AccessPath parse(String l) {
+    return stringToAccessPath(this.method.getActiveBody().getLocals(), l);
+  }
 
 
-	private static AccessPath findFieldsAndLocals(
-			Chain<Local> locals, String arg) {
-		
-		String[] base = arg.split("\\.");
-		Local findLocal = AliasTest.findSingleLocal(locals, base[0]);
-		if(base.length == 1){
-			return new AccessPath(findLocal, findLocal.getType());
-		}
-		String[] splitted = new String[base.length-1];
-		System.arraycopy(base, 1, splitted, 0, base.length-1);
-		Type t = null;
-		if(findLocal != null)
-			t = findLocal.getType();
-		LinkedList<FieldWithStmt> fields = new LinkedList<FieldWithStmt>();
-		for(int i = 0; i < splitted.length; i++){
-			if (t instanceof RefType || t == null) {
-				RefType refType = (RefType) t;
-//				SootClass sootClass = refType.getSootClass();
-				SootField fieldByName = null;
-				if(splitted[i].startsWith("<")){
-					fieldByName = Scene.v().getField(splitted[i]);
-				} else{
-					fieldByName = refType.getSootClass()
-						.getFieldByName(splitted[i]);
-				}
-				fields.add(new FieldWithStmt(fieldByName,fieldByName.getType(),null));
-				t = fieldByName.getType();
-			} else if(splitted[i].equals("array")){
-				fields.add(new FieldWithStmt(AliasFinder.ARRAY_FIELD, AliasFinder.ARRAY_FIELD.getType(),null));
-			} 
-			else {
-				throw new RuntimeException("Parsing of fields of locals failed");
-			}
-			
-		} 
-		FieldWithStmt[] array = new FieldWithStmt[fields.size()];
-		array = fields.toArray(array);
-		return new AccessPath(findLocal,findLocal.getType(), array);
-	}
+  @Override
+  protected int getPointsToSize() {
+    createDart();
+    AliasResults res1 =
+        dart.findAliasAtStmt(parse(testVariable), testStmt,
+            new AllCallersRequester<BiDiInterproceduralCFG<Unit, SootMethod>>(cfg));
+    if (res1 == null)
+      return 0;
+    Set<Unit> allocSites = new HashSet<>();
+    for (Pair<Unit, AccessPath> o : res1.keySet()) {
+      allocSites.add(o.getO2().getSourceStmt());
+    }
+    return allocSites.size();
+  }
+
+
+  public static AccessPath stringToAccessPath(Chain<Local> locals, String arg) {
+    for (Local l : locals) {
+      if (arg.contains(".")) {
+        return findFieldsAndLocals(locals, arg);
+      } else if (l.toString().equals(arg)) {
+        return new AccessPath(l, l.getType());
+      }
+    }
+
+    throw new RuntimeException("Could not parse String to access path");
+  }
+
+
+  private static AccessPath findFieldsAndLocals(Chain<Local> locals, String arg) {
+
+    String[] base = arg.split("\\.");
+    Local findLocal = AliasTest.findSingleLocal(locals, base[0]);
+    if (base.length == 1) {
+      return new AccessPath(findLocal, findLocal.getType());
+    }
+    String[] splitted = new String[base.length - 1];
+    System.arraycopy(base, 1, splitted, 0, base.length - 1);
+    Type t = null;
+    if (findLocal != null)
+      t = findLocal.getType();
+    LinkedList<FieldWithStmt> fields = new LinkedList<FieldWithStmt>();
+    for (int i = 0; i < splitted.length; i++) {
+      if (t instanceof RefType || t == null) {
+        RefType refType = (RefType) t;
+        // SootClass sootClass = refType.getSootClass();
+        SootField fieldByName = null;
+        if (splitted[i].startsWith("<")) {
+          fieldByName = Scene.v().getField(splitted[i]);
+        } else {
+          fieldByName = refType.getSootClass().getFieldByName(splitted[i]);
+        }
+        fields.add(new FieldWithStmt(fieldByName, fieldByName.getType(), null));
+        t = fieldByName.getType();
+      } else if (splitted[i].equals("array")) {
+        fields.add(new FieldWithStmt(AliasFinder.ARRAY_FIELD, AliasFinder.ARRAY_FIELD.getType(),
+            null));
+      } else {
+        throw new RuntimeException("Parsing of fields of locals failed");
+      }
+
+    }
+    FieldWithStmt[] array = new FieldWithStmt[fields.size()];
+    array = fields.toArray(array);
+    return new AccessPath(findLocal, findLocal.getType(), array);
+  }
 }
--- AliasBenchmark/src/tests/experiment/Main.java
+++ AliasBenchmark/src/tests/experiment/Main.java
@@ -27,270 +27,234 @@ import benchmark.internal.ExprResult;
 import benchmark.internal.QueryInfo;
 
 public class Main {
-	public final static String[] testcases = new String[]{
-		"basic.SimpleAlias1",
-		"basic.Loops1",
-		"basic.Loops2",
-		"basic.Method1",
-		"basic.Method2",
-		"basic.Parameter1",
-		"basic.Parameter2",
-		"basic.Recursion1",
-		"basic.ReturnValue1",
-		"basic.ReturnValue2",
-		"basic.ReturnValue3",
-		"basic.Branching1",
-
-		"collections.Array1",
-		"collections.Array2",
-		"collections.List1",
-		"collections.List2",
-		"collections.Map1",
-		"collections.Set1",
-		
-		"cornerCases.AccessPath1",
-		"cornerCases.ContextSensitivity1",
-		"cornerCases.ContextSensitivity2",
-		"cornerCases.ContextSensitivity3",
-		"cornerCases.FieldSensitivity1",
-		"cornerCases.FieldSensitivity2",
-		"cornerCases.FlowSensitivity1",
-		"cornerCases.ObjectSensitivity1",
-		"cornerCases.ObjectSensitivity2",
-		"cornerCases.StrongUpdate1",
-		"cornerCases.StrongUpdate2",
-		
-		"generalJava.Exception1",
-		"generalJava.Exception2",
-		"generalJava.Interface1",
-		"generalJava.Null1",
-		"generalJava.Null2",
-		"generalJava.OuterClass1",
-		"generalJava.StaticVariables1",
-		"generalJava.SuperClasses1",
-	};
-
-	
-	private static int globalTP = 0;
-	private static int globalManuFN = 0;
-	private static int globalDartFN = 0;
-	private static int globalYanFN = 0;
-	private static int globalManuFP = 0;
-	private static int globalDartFP = 0;
-	private static int globalYanFP = 0;
-
-	private static int globalPTSTP = 0;
-	private static int globalPTSREPDART = 0;
-	private static int globalPTSREPMANU = 0;
-	
-	public static void main(String...args) throws IOException {
-		final FileWriter writer = new FileWriter("comparison-experiment.csv");
-		writer.write("Testcase;Gt;ptsGt;Yan;Manu;ptsRepManu;Dart;ptsRepDart\n");
-		for(final String testcase : testcases){
-			initializeSoot(testcase);
-			Transform callConstantTransformer = new Transform("wjtp.preparation",
-					new SceneTransformer() {
-	
-						@Override
-						protected void internalTransform(String phaseName,
-								Map<String, String> options) {
-							try {
-								QueryInfo queryInfo = retrieveQueryInfo();
-								System.out.println(queryInfo);
-								int testTP = queryInfo.getTruePositives().size();
-								int ptsTP = queryInfo.computeExecpectedPointsToSize();
-								globalTP += testTP;
-								globalPTSTP += ptsTP;
-								writer.write(testcase+";"+testTP+";"+ptsTP+";");
-								int fp;
-								int fn;
-								ExprResult res;
-								try{
-									YanEvaluator yan = new YanEvaluator(queryInfo);
-									res = yan.evaluateAlias();
-									fp = res.getFalsePositive().size();
-									fn = res.getFalseNegatives().size();
-								}catch (AccessPathNotSupportedException e){
-									fn = testTP;
-									fp = 0;
-								}
-								globalYanFN += fn;
-								globalYanFP += fp;
-								writer.write(parse((testTP-fn),"\\TP")+parse((fp),"\\FP")+parse((fn),"\\FN")+";");
-
-								int ptsrep = 0;
-								try{
-									ManuEvaluator manu = new ManuEvaluator(queryInfo);
-									res = manu.evaluateAlias();
-									fp = res.getFalsePositive().size();
-									fn = res.getFalseNegatives().size();
-									ptsrep = res.getPointsToSetSize();
-								} catch (AccessPathNotSupportedException e){
-									fn = testTP;
-									fp = 0;
-								}
-								globalManuFN += fn;
-								globalManuFP += fp;
-								globalPTSREPMANU += ptsrep;
-								writer.write(parse((testTP-fn),"\\TP")+parse((fp),"\\FP")+parse((fn),"\\FN")+";" + ptsrep+";");
-								DartEvaluator dart = new DartEvaluator(queryInfo);
-								res = dart.evaluateAlias();
-								fp = res.getFalsePositive().size();
-								fn = res.getFalseNegatives().size();
-								ptsrep = res.getPointsToSetSize();
-								globalDartFN += fn;
-								globalDartFP += fp;
-								globalPTSREPDART += ptsrep;
-								writer.write(parse((testTP-fn),"\\TP")+parse((fp),"\\FP")+parse((fn),"\\FN")+";" + ptsrep+";\n");
-								
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-
-						private String parse(int j, String string) {
-							String s = "";
-							for(int i = 0; i < j; i++){
-								s += " " +string;
-							}
-							return s;
-						}
-	
-					});
-	
-	
-	
-			PackManager.v().getPack("cg").apply();
-			PackManager.v().getPack("wjtp").add(callConstantTransformer);
-			PackManager.v().getPack("wjtp").apply();
-		}
-		float manuRecall = recall(globalTP, globalManuFN);
-		float manuPrecision = precision(globalTP, globalManuFP);
-		
-		float dartRecall = recall(globalTP, globalDartFN);
-		float dartPrecision = precision(globalTP, globalDartFP);
-		
-		float yanRecall = recall(globalTP, globalYanFN);
-		float yanPrecision = precision(globalTP, globalYanFP);
-		
-		writer.write("precision_yan=" + yanPrecision+",");
-		writer.write("recall_yan=" + yanRecall+"\n");
-
-		writer.write("precision_manu=" + manuPrecision+",");
-		writer.write("recall_manu=" + manuRecall+"\n");
-
-		writer.write("precision_dart=" + dartPrecision+",");
-		writer.write("recall_dart=" + dartRecall+"\n");
-		
-		
-		
-		writer.close();
-	}
+  public final static String[] testcases = new String[] {
+      // "basic.SimpleAlias1", "basic.Loops1",
+      // "basic.Loops2", "basic.Method1", "basic.Method2", "basic.Parameter1", "basic.Parameter2",
+      // "basic.Recursion1", "basic.ReturnValue1", "basic.ReturnValue2", "basic.ReturnValue3",
+      // "basic.Branching1",
+      //
+      // "cornerCases.AccessPath1", "cornerCases.ContextSensitivity1",
+      // "cornerCases.ContextSensitivity2", "cornerCases.ContextSensitivity3",
+      // "cornerCases.FieldSensitivity1", "cornerCases.FieldSensitivity2",
+      // "cornerCases.FlowSensitivity1",
+      "cornerCases.ObjectSensitivity1", "cornerCases.ObjectSensitivity2",
+  // "cornerCases.StrongUpdate1", "cornerCases.StrongUpdate2",
+  //
+  // "generalJava.Exception1", "generalJava.Exception2", "generalJava.Interface1",
+  // "generalJava.Null1", "generalJava.Null2", "generalJava.OuterClass1",
+  // "generalJava.StaticVariables1", "generalJava.SuperClasses1", "collections.Array1",
+  // "collections.List1", "collections.List2", "collections.Map1", "collections.Set1"
+      };
 
 
-	private static float recall(int tp, int fn) {
-		return ((float) tp) /(tp+fn);
-	}
+  private static int globalTP = 0;
+  private static int globalManuFN = 0;
+  private static int globalDartFN = 0;
+  private static int globalYanFN = 0;
+  private static int globalManuFP = 0;
+  private static int globalDartFP = 0;
+  private static int globalYanFP = 0;
 
-	private static float precision(int tp, int fp) {
-		return ((float) tp) /(tp+fp);
-	}
-	
-	protected static QueryInfo retrieveQueryInfo() {
-		String alloc = null;
-		QueryInfo queryInfo = new QueryInfo();
-		for (SootClass sc : Scene.v().getClasses()) {
-			for (SootMethod sm : sc.getMethods()) {
-				if(!sm.hasActiveBody())
-					continue;
-				Body methodBody = sm.retrieveActiveBody();
-				for (Unit u : methodBody.getUnits()) {
-					Stmt s = (Stmt) u;
+  private static int globalPTSTP = 0;
+  private static int globalPTSREPDART = 0;
+  private static int globalPTSREPMANU = 0;
 
-					if (s.containsInvokeExpr()) {
-						InvokeExpr ie = s.getInvokeExpr();
-						SootMethod callee = ie.getMethod();
+  public static void main(String... args) throws IOException {
+    final FileWriter writer = new FileWriter("comparison-experiment.csv");
+    writer.write("Testcase;Gt;ptsGt;Yan;Manu;ptsRepManu;Dart;ptsRepDart;\n");
+    for (final String testcase : testcases) {
+      initializeSoot(testcase);
+      Transform callConstantTransformer = new Transform("wjtp.preparation", new SceneTransformer() {
 
-						if (callee
-								.getDeclaringClass()
-								.getName()
-								.equals("benchmark.internal.Benchmark")) {
+        @Override
+        protected void internalTransform(String phaseName, Map<String, String> options) {
+          try {
+            QueryInfo queryInfo = retrieveQueryInfo();
+            System.out.println(queryInfo);
+            int testTP = queryInfo.getTruePositives().size();
+            int ptsTP = queryInfo.computeExecpectedPointsToSize();
+            globalTP += testTP;
+            globalPTSTP += ptsTP;
+            writer.write(testcase + ";" + testTP + ";" + ptsTP + ";");
+            int fp;
+            int fn;
+            ExprResult res;
+            try {
+              YanEvaluator yan = new YanEvaluator(queryInfo);
+              res = yan.evaluateAlias();
+              fp = res.getFalsePositive().size();
+              fn = res.getFalseNegatives().size();
+            } catch (AccessPathNotSupportedException e) {
+              fn = testTP;
+              fp = 0;
+            }
+            globalYanFN += fn;
+            globalYanFP += fp;
+            writer.write(parse((testTP - fn), "\\TP") + parse((fp), "\\FP") + parse((fn), "\\FN")
+                + ";");
 
-							if (callee.getName().equals(
-									"test")) {
-								String variableToTest = ie
-										.getArg(0)
-										.toString();
-								String results = ie.getArg(
-										1).toString();
-								queryInfo.registerTestInfo(
-										s, sm,variableToTest,
-										results);
+            int ptsrep = 0;
+            try {
+              ManuEvaluator manu = new ManuEvaluator(queryInfo);
+              res = manu.evaluateAlias();
+              fp = res.getFalsePositive().size();
+              fn = res.getFalseNegatives().size();
+              ptsrep = res.getPointsToSetSize();
+            } catch (AccessPathNotSupportedException e) {
+              fn = testTP;
+              fp = 0;
+            }
+            globalManuFN += fn;
+            globalManuFP += fp;
+            globalPTSREPMANU += ptsrep;
+            writer.write(parse((testTP - fn), "\\TP") + parse((fp), "\\FP") + parse((fn), "\\FN")
+                + ";" + parse(ptsTP, "\\TP") + parse((ptsrep - ptsTP), "\\FP") + ";");
+            DartEvaluator dart = new DartEvaluator(queryInfo);
+            res = dart.evaluateAlias();
+            fp = res.getFalsePositive().size();
+            fn = res.getFalseNegatives().size();
+            ptsrep = res.getPointsToSetSize();
+            globalDartFN += fn;
+            globalDartFP += fp;
+            globalPTSREPDART += ptsrep;
+            writer.write(parse((testTP - fn), "\\TP") + parse((fp), "\\FP") + parse((fn), "\\FN")
+                + ";" + parse(ptsTP, "\\TP") + parse((ptsrep - ptsTP), "\\FP") + ";\n");
 
-							} else if (callee.getName()
-									.equals("alloc")) {
-								alloc = ie.getArgs().get(0)
-										.toString();
-							}
-						}
-					} else if (s instanceof AssignStmt
-							&& alloc != null) {
-						queryInfo.registerAllocationSite(s,
-								alloc);
-						alloc = null;
-					}
-				}
-			}
-		}
-		return queryInfo;
-	}
+          } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+        }
 
-	
-	private static void initializeSoot(String testcase) {
-		G.v().reset();
-		Options.v().set_whole_program(true);
-		Options.v().setPhaseOption("jb", "use-original-names:true");
-		Options.v().setPhaseOption("cg.spark", "on");
-		Options.v().set_output_format(Options.output_format_none);
-		String userdir = System.getProperty("user.dir");
-		String sootCp = userdir + "/bin";
-		Options.v().set_prepend_classpath(true);
-		Options.v().setPhaseOption("cg", "trim-clinit:false");
-		Options.v().set_no_bodies_for_excluded(true);
-		Options.v().set_allow_phantom_refs(true);
-		
-		List<String> includeList = new LinkedList<String>();
-		includeList.add("java.lang.*");
-		includeList.add("java.util.*");
-		includeList.add("java.io.*");
-		includeList.add("sun.misc.*");
-		includeList.add("java.net.*");
-		includeList.add("javax.servlet.*");
-		includeList.add("javax.crypto.*");
+        private String parse(int j, String string) {
+          String s = "";
+          for (int i = 0; i < j; i++) {
+            s += " " + string;
+          }
+          return s;
+        }
 
-		includeList.add("android.*");
-		includeList.add("org.apache.http.*");
+      });
 
-		includeList.add("de.test.*");
-		includeList.add("soot.*");
-		includeList.add("com.example.*");
-		includeList.add("libcore.icu.*");
-		includeList.add("securibench.*");
-		Options.v().set_include(includeList);
-	
-		Options.v().set_soot_classpath(sootCp);
-		Options.v().set_main_class(testcase);
-		
-		Scene.v().addBasicClass(testcase, SootClass.BODIES);
-		Scene.v().loadNecessaryClasses();
-		SootClass c = Scene.v().forceResolve(testcase, SootClass.BODIES);
-		if (c != null){
-			c.setApplicationClass();
-		}
-		SootMethod methodByName = c.getMethodByName("main");
-		List<SootMethod> ePoints = new LinkedList<>();
-		ePoints.add(methodByName);
-		Scene.v().setEntryPoints(ePoints);
-	}
+
+
+      PackManager.v().getPack("cg").apply();
+      PackManager.v().getPack("wjtp").add(callConstantTransformer);
+      PackManager.v().getPack("wjtp").apply();
+    }
+    float manuRecall = recall(globalTP, globalManuFN);
+    float manuPrecision = precision(globalTP, globalManuFP);
+
+    float dartRecall = recall(globalTP, globalDartFN);
+    float dartPrecision = precision(globalTP, globalDartFP);
+
+    float yanRecall = recall(globalTP, globalYanFN);
+    float yanPrecision = precision(globalTP, globalYanFP);
+
+    writer.write("precision_yan=" + yanPrecision + ",");
+    writer.write("recall_yan=" + yanRecall + "\n");
+
+    writer.write("precision_manu=" + manuPrecision + ",");
+    writer.write("recall_manu=" + manuRecall + "\n");
+
+    writer.write("precision_dart=" + dartPrecision + ",");
+    writer.write("recall_dart=" + dartRecall + "\n");
+
+
+
+    writer.close();
+  }
+
+
+  private static float recall(int tp, int fn) {
+    return ((float) tp) / (tp + fn);
+  }
+
+  private static float precision(int tp, int fp) {
+    return ((float) tp) / (tp + fp);
+  }
+
+  protected static QueryInfo retrieveQueryInfo() {
+    String alloc = null;
+    QueryInfo queryInfo = new QueryInfo();
+    for (SootClass sc : Scene.v().getClasses()) {
+      for (SootMethod sm : sc.getMethods()) {
+        if (!sm.hasActiveBody())
+          continue;
+        Body methodBody = sm.retrieveActiveBody();
+        for (Unit u : methodBody.getUnits()) {
+          Stmt s = (Stmt) u;
+
+          if (s.containsInvokeExpr()) {
+            InvokeExpr ie = s.getInvokeExpr();
+            SootMethod callee = ie.getMethod();
+
+            if (callee.getDeclaringClass().getName().equals("benchmark.internal.Benchmark")) {
+
+              if (callee.getName().equals("test")) {
+                String variableToTest = ie.getArg(0).toString();
+                String results = ie.getArg(1).toString();
+                queryInfo.registerTestInfo(s, sm, variableToTest, results);
+
+              } else if (callee.getName().equals("alloc")) {
+                alloc = ie.getArgs().get(0).toString();
+              }
+            }
+          } else if (s instanceof AssignStmt && alloc != null) {
+            queryInfo.registerAllocationSite(s, alloc);
+            alloc = null;
+          }
+        }
+      }
+    }
+    return queryInfo;
+  }
+
+
+  private static void initializeSoot(String testcase) {
+    G.v().reset();
+    Options.v().set_whole_program(true);
+    Options.v().setPhaseOption("jb", "use-original-names:true");
+    Options.v().setPhaseOption("cg.spark", "on");
+    Options.v().set_output_format(Options.output_format_none);
+    String userdir = System.getProperty("user.dir");
+    String sootCp = userdir + "/bin";
+    Options.v().set_prepend_classpath(true);
+    Options.v().setPhaseOption("cg", "trim-clinit:false");
+    Options.v().set_no_bodies_for_excluded(true);
+    Options.v().set_allow_phantom_refs(true);
+
+    List<String> includeList = new LinkedList<String>();
+    includeList.add("java.lang.*");
+    includeList.add("java.util.*");
+    includeList.add("java.io.*");
+    includeList.add("sun.misc.*");
+    includeList.add("java.net.*");
+    includeList.add("javax.servlet.*");
+    includeList.add("javax.crypto.*");
+
+    includeList.add("android.*");
+    includeList.add("org.apache.http.*");
+
+    includeList.add("de.test.*");
+    includeList.add("soot.*");
+    includeList.add("com.example.*");
+    includeList.add("libcore.icu.*");
+    includeList.add("securibench.*");
+    Options.v().set_include(includeList);
+
+    Options.v().set_soot_classpath(sootCp);
+    Options.v().set_main_class(testcase);
+
+    Scene.v().addBasicClass(testcase, SootClass.BODIES);
+    Scene.v().loadNecessaryClasses();
+    SootClass c = Scene.v().forceResolve(testcase, SootClass.BODIES);
+    if (c != null) {
+      c.setApplicationClass();
+    }
+    SootMethod methodByName = c.getMethodByName("main");
+    List<SootMethod> ePoints = new LinkedList<>();
+    ePoints.add(methodByName);
+    Scene.v().setEntryPoints(ePoints);
+  }
 }
